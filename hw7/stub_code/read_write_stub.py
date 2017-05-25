56d912e8c695e7efdda6f855092d49c0e487eff8
bomicbon <urianlee@yahoo.com>
2017-05-25 11:15:09 -0700

    gets tree now

--- hw7/stub_code/read_write_stub.py
+++ hw7/stub_code/read_write_stub.py
@@ -1,5 +1,7 @@
-import zipfile, os
-import re, nltk
+import zipfile
+import os
+import re
+import nltk
 from collections import OrderedDict
 
 ###############################################################################
@@ -8,7 +10,10 @@ from collections import OrderedDict
 
 # returns a dictionary where the question numbers are the key
 # and its items are another dict of difficulty, question, type, and answer
-# e.g. story_dict = {'fables-01-1': {'Difficulty': x, 'Question': y, 'Type':}, 'fables-01-2': {...}, ...}
+# e.g. story_dict = {'fables-01-1': {'Difficulty': x, 'Question': y,
+# 'Type':}, 'fables-01-2': {...}, ...}
+
+
 def getQA(filename):
     content = open(filename, 'rU', encoding='latin1').read()
     question_dict = {}
@@ -21,6 +26,7 @@ def getQA(filename):
         question_dict[qid]['Type'] = m.group("type")
     return question_dict
 
+
 def get_data_dict(fname):
     data_dict = {}
     data_types = ["story", "sch", "questions"]
@@ -28,15 +34,18 @@ def get_data_dict(fname):
     for dt in data_types:
         data_dict[dt] = read_file(fname + "." + dt)
         for tp in parser_types:
-            data_dict['{}.{}'.format(dt, tp)] = read_file(fname + "." + dt + "." + tp)
+            data_dict['{}.{}'.format(dt, tp)] = read_file(
+                fname + "." + dt + "." + tp)
     return data_dict
 
+
 # Read the file from disk
-# filename can be fables-01.story, fables-01.sch, fables-01-.story.dep, fables-01.story.par
+# filename can be fables-01.story, fables-01.sch, fables-01-.story.dep,
+# fables-01.story.par
 def read_file(filename):
     fh = open(filename, 'r')
     text = fh.read()
-    fh.close()   
+    fh.close()
     return text
 
 ###############################################################################
@@ -44,47 +53,48 @@ def read_file(filename):
 ###############################################################################
 
 
-    
 #######################################################################
 
 if __name__ == '__main__':
 
     # Loop over the files in fables and blogs in order.
     output_file = open("train_my_answers.txt", "w", encoding="utf-8")
-    cname_size_dict = OrderedDict();
-    cname_size_dict.update({"fables":2})
-    cname_size_dict.update({"blogs":1})
+    cname_size_dict = OrderedDict()
+    cname_size_dict.update({"fables": 2})
+    cname_size_dict.update({"blogs": 1})
     for cname, size in cname_size_dict.items():
-        for i in range (0, size):
+        for i in range(0, size):
             # File format as fables-01, fables-11
-            fname = "{0}-{1:02d}".format(cname, i+1)
+            fname = "{0}-{1:02d}".format(cname, i + 1)
             #print("File Name: " + fname)
             data_dict = get_data_dict(fname)
 
             questions = getQA("{}.questions".format(fname))
             for j in range(0, len(questions)):
-                qname = "{0}-{1}".format(fname, j+1)
+                qname = "{0}-{1}".format(fname, j + 1)
                 if qname in questions:
                     print("QuestionID: " + qname)
                     question = questions[qname]['Question']
                     print(question)
                     qtypes = questions[qname]['Type']
-                    
-		    # Read the content of fname.questions.par, fname.questions.dep for hint.
+
+                    # Read the content of fname.questions.par,
+                    # fname.questions.dep for hint.
                     question_par = data_dict["questions.par"]
                     question_dep = data_dict["questions.dep"]
-		    
+
                     answer = None
                     # qtypes can be "Story", "Sch", "Sch | Story"
                     for qt in qtypes.split("|"):
                         qt = qt.strip().lower()
-                        # These are the text data where you can look for answers.
+                        # These are the text data where you can look for
+                        # answers.
                         raw_text = data_dict[qt]
                         par_text = data_dict[qt + ".par"]
                         dep_text = data_dict[qt + ".dep"]
                         # TODO: You need to find the answer for this question.
                         answer = None
-                                            
+
                     print("Answer: " + str(answer))
                     print("")
 
@@ -92,8 +102,3 @@ if __name__ == '__main__':
                     output_file.write("QuestionID: {}\n".format(qname))
                     output_file.write("Answer: {}\n\n".format(answer))
     output_file.close()
-
-                    
-
-
-
